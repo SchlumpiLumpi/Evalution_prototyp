1c8058b416e11573a9ab756ae721b90e5a497429
SchlumpiLumpi <Ba_Evaluierung@proton.me>
2025-04-03 13:11:31 +0200

    updated routing

--- app.js
+++ app.js
@@ -4,53 +4,71 @@ import OpenAI from "openai"
 import cors from "cors"
 import fs from "fs/promises"
 
-//read data from file
-async function getReferenceData(filepath) {
-    try {
-        const data = await fs.readFile(filepath, "utf-8")
-        return JSON.parse(data)
-    }
-    catch (error) {
-        console.log('something went wrong', error)
-        return null
-    }
-}
-const referenceData = await getReferenceData("public/data/referenceData/scatter_dataset.json")
-const referenceDataString = JSON.stringify(referenceData) 
-// console.log(referenceData)
-
 
 //app
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
+app.set("view engine","ejs")
 
 //instaciate OpenAI
 const openAI = new OpenAI({
     apiKey:  'your_apiKey'
 })
- 
+//default message and context (data)
+let messages = [{
+    role: "system",
+    content: 'You are a helpful assistant'
+}];
 // set ApiKey via text input
 app.post("/apiKey",async(req,res) => {
     const apiKey = req.body.apiKey.toString()
     openAI.apiKey = apiKey
 })
 
+//basic routing
+app.get("/",async (req,res)=>{
+    //clear instructions
+    messages=[]
+    //load reference data
+    const referenceData = await getReferenceData("public/data/referenceData/refA.json")
+    const referenceDataString = JSON.stringify(referenceData) 
+    //set instructions
+    messages.push({
+        role: "system",
+        // content: `You are helping generating insights for this reference data: ${referenceDataString}.
+        //           You keep your answers short and well structured. If you provide additional information or hypothesizing about the dataset,
+        //           at the end of your response add at least 2 and at most 3 reliable ressources of your claims`
+        content: 'You always reply with the response: A'
+    })
+        
+    res.render("index")
+})
+app.get("/da", async (req,res) => {
+    //clear instructions
+    messages=[]
+    //load reference data
+    const referenceData = await getReferenceData("public/data/referenceData/refB.json")
+    const referenceDataString = JSON.stringify(referenceData) 
+    //set instructions
+    messages.push({
+        role: "system",
+        // content: `You are helping generating insights for this reference data: ${referenceDataString}.
+        //           You keep your answers short and well structured. If you provide additional information or hypothesizing about the dataset,
+        //           at the end of your response add at least 2 and at most 3 reliable ressources of your claims`
+        content: 'You always reply with the response: A'
+    })
+    res.render("da")
+})
 
-//give context
-const messages = [{
-    role: "system",
-    // content: `reference data: ${JSON.stringify(referenceData)}`,
-    // content: `You are a helpful assistant. You are helping generating insights for this data ${referenceDataString}`
-    content: 'You are a helpful assistant'
-}];
 
-//communication with chat component of index.html
+//Chat Component
 app.post("/chat", async (req, res) => {
+    // receive user input
     const userMessage = req.body.message;
     messages.push({ role: "user", content: userMessage });
-
+    // receive and send OpenAI response 
     try {
         const completion = await openAI.chat.completions.create({
             model: "gpt-4o",
@@ -65,7 +83,21 @@ app.post("/chat", async (req, res) => {
         res.status(500).json({ error: "Something went wrong!" });
     }
 });
-//LocalHost
 
+
+//deployment port (render env port) || local port
 const PORT = process.env.PORT || 3000 
-app.listen(PORT, () => console.log("Server running on http://localhost:3000"));
+app.listen(PORT, () => console.log("Server running on PORT:", PORT));
+
+
+//read data from file
+async function getReferenceData(filepath) {
+    try {
+        const data = await fs.readFile(filepath, "utf-8")
+        return JSON.parse(data)
+    }
+    catch (error) {
+        console.log('something went wrong', error)
+        return null
+    }
+}
